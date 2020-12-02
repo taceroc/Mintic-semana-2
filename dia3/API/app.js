aa05084e74155e5fc4643268c3dfc14c15ea5768
taceroc <taceroc@unal.edu.co>
2020-12-02 08:17:10 -0500

    add json con lineas que muestrane en la pantalla

--- dia3/API/app.js
+++ dia3/API/app.js
@@ -11,13 +11,13 @@ const getSearchText = () =>{
 
         const movieSearchTitle = document.getElementById('movieSearchTitle').value ? document.getElementById('movieSearchTitle').value : false ;
         console.log(movieSearchTitle);
-
-
+        
+        
         testApi(movieSearchTitle);
 
     });
 
-    console.log(submit);
+    //console.log(submit);
 }
 
 getSearchText();
@@ -31,9 +31,58 @@ const testApi = async (movieSearchTitle) =>{
     let resultJson;
     if(resultText.ok){
         resultJson = await resultText.json();
-        console.log(resultJson);
+        // console.log(resultJson);
         console.log(resultJson.Poster);
-    };
+        printOnScreen(resultJson);
+    }
+    return resultJson;
 } 
 
-testApi();
+//para mostrar en pantalla en el html
+
+const printOnScreen = (dataText) => {
+
+    // dataText.forEach(element => {
+    //     console.log(dataText;
+    // });
+    // let testArray = Array.from(dataText);
+    // console.log(testArray);
+    // let movieArray = [];
+    // for (let key in dataText) {
+    //     movieArray.push(key + ': ' + dataText[key]);
+    // }
+    // console.log(movieArray);
+
+    const movieContainer = document.querySelector('#movieContainer');
+
+    const {Title, Actors, Year, Poster} = dataText;
+
+    console.log(Title, Actors, Year, Poster);
+
+    const moviePoster = document.createElement('img');
+    moviePoster.className = "card-img-top";
+    moviePoster.src = Poster;
+    moviePoster.alt = 'poster ' + Title;
+
+    const movieTitle = document.createElement('h2');
+    movieTitle.textContent = Title;
+
+    const movieActors = document.createElement('li');
+    movieActors.textContent = Actors;
+
+    const movieYear = document.createElement('li');
+    movieYear.textContent = Year;
+    
+
+    movieContainer.appendChild(moviePoster);
+    movieContainer.appendChild(movieTitle);
+    movieContainer.appendChild(movieActors);
+    movieContainer.appendChild(movieYear);
+
+
+    
+    
+
+}
+
+//testApi();
